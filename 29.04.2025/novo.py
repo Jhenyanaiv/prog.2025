1cad8a3e8f95097b2a7427d7300b9c7eb0361d27
jhenyanaiv <jhenyfferviana2@gmail.com>
2025-04-29 18:00:55 -0300

    aulas calculos media

--- 29.04.2025/novo.py
+++ 29.04.2025/novo.py
@@ -1,1 +1,10 @@
 print("Calcular area do circulo ")
+
+
+raio = 5.13
+
+area = 3.14 * raio ** 2
+
+print ("area do circulo" ,area)
+
+print  (f'a area do circulo do raio {raio} é {area: .2f}' )
---
+++ 29.04.2025/novo1.py
@@ -0,0 +1,9 @@
+print (" Calculo da média aritimetica ")
+
+n1 = 6.5
+n2 = 7 
+n3 = 4.9 
+n4 = 6.3 
+
+media = ( n1+n2+n3+n4)/4 
+print (media)
