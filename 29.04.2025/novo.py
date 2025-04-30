1dd3c8a507a6f70703824585cba2f032c7c5d744
jhenyanaiv <jhenyfferviana2@gmail.com>
2025-04-30 17:49:17 -0300

    aula

--- 29.04.2025/novo.py
+++ 29.04.2025/novo.py
@@ -1,10 +1,15 @@
 print("Calcular area do circulo ")
 
 
-raio = 5.13
+raio = float (input('digite o raio') )
 
-area = 3.14 * raio ** 2
+area = 3.14 * (raio) ** 2
 
-print ("area do circulo" ,area)
+print ("area do circulo {area:.3f}")
 
 print  (f'a area do circulo do raio {raio} é {area: .2f}' )
+
+comprimento = 2 * 3.14 *   (raio) 
+
+
+print (f'o comp da circu {comprimento:.3f} ')
---
+++ 29.04.2025/novo4.py
@@ -0,0 +1,14 @@
+# calcular 
+
+#1 informe duas notas
+# calcular media 
+# exibir media 
+
+
+nota1 = int (input('digite sua nota 1 '))
+
+nota2 = int (input('digite sua nota 2 '))
+
+media = int(nota1 * 2 + nota2 * 3)/5
+
+print(f'sua nota é {media:.0f}')
