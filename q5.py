257b9499e404cba821152fce2895d08e6836120e
JhenyAnaiv <jhenyfferviana2@gmail.com>
2025-05-22 09:59:19 -0300

    22/05

--- q5.py
+++ q5.py
@@ -2,3 +2,21 @@ print("informe  tempo que um veículo permaneceu no estacionamento" )
 h = int (input("H: "))
 m = int (input("M: "))
 
+totalm = h * 60 + m
+
+if totalm % 60 ==0:
+    hp = totalm // 60
+else :
+    hp = totalm // 60 + 1
+
+if hp > 12 :
+    valor = 30 
+else :
+    if hp <= 2 :
+        valor = hp * 8 
+    elif hp <= 4 :
+        valor = 2 * 8 + (hp - 2) * 5
+    else :
+        valor = 2 * 8 + 2 * 5 + (hp - 4) * 3
+
+        print("o valor a ser pago " ,valor ,",00")
