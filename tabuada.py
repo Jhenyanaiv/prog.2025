1376e2f85e2edbe31d0f45e50a78bece93ab4d71
JhenyAnaiv <jhenyfferviana2@gmail.com>
2025-05-27 17:05:36 -0300

    27/05

---
+++ tabuada.py
@@ -0,0 +1,11 @@
+import sys
+
+mult = int (input("informe o multiplicador"))
+    if v <=0:
+        sys.exit('informe valor positivo :')
+
+except ValueError:
+    sys.exit("digite apenas números reais positivos")
+
+ mult1 = mult * 1
+    print("{mult1}")
